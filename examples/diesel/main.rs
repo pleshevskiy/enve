ddf7f58e932d7c8dcd29fa8b483f86bfe8bf77ea
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-07-21 20:10:31 +0300

    chore: refac examples

--- examples/diesel/main.rs
+++ examples/diesel/main.rs
@@ -1,16 +1,12 @@
 #[macro_use]
 extern crate diesel;
 
-mod db;
-mod models;
 mod schema;
 
-use crate::models::*;
 use diesel::prelude::*;
 use dotenv::dotenv;
-use itconfig::config;
 
-config! {
+itconfig::config! {
     DATABASE_URL,
 }
 
@@ -18,7 +14,7 @@ fn main() {
     dotenv().expect("dotenv setup to be successful");
     config::init();
 
-    let connection = db::establish_connection();
+    let connection = establish_connection();
     let posts = get_posts(&connection);
 
     println!("Displaying {} posts", posts.len());
@@ -38,4 +34,18 @@ fn get_posts(connection: &PgConnection) -> Vec<Post> {
         .limit(5)
         .get_results::<Post>(connection)
         .expect("Error loading posts")
+}
+
+fn establish_connection() -> PgConnection {
+    let database_url = config::DATABASE_URL();
+    PgConnection::establish(database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[derive(Queryable)]
+struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
 }
--- examples/diesel/src/models.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Queryable)]
-pub struct Post {
-    pub id: i32,
-    pub title: String,
-    pub body: String,
-    pub published: bool,
-}
